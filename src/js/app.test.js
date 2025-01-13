78debcc710f8d8f44f6739a46aa29089d256441e
Aleksandra Ivanova <sashaivanova90@mail.ru>
2025-01-13 16:49:58 +0300

    change README.md, app.test.js

---
+++ src/js/app.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { getRandomInt } from "../js/app"
+
+
+
+test('должна возвращать число в указанном диапазоне', () => {
+  const min = 0;
+  const max = 15;
+  const result = getRandomInt(min, max);
+  expect(result).toBeGreaterThanOrEqual(min);
+  expect(result).toBeLessThanOrEqual(max);
+  });
